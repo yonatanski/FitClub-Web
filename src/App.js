dc00677cacd2023e91cf0b3a8a382c1f69c6526f
yonatanski <phpcodeyg@gmail.com>
2022-07-28 07:55:09 +0200

    Hero Component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css"
+import Hero from "./components/Hero/Hero"
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+      <Hero />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero__leftH">LEFT SIDE</div>
+      <div className="hero__rightH">RIGHT SIDE</div>
+    </div>
+  )
+}
+
+export default Hero
