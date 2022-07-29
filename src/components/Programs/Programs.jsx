f478b5e7d82b10799492fe951fe081d038fcc7ab
yonatanski <phpcodeyg@gmail.com>
2022-07-29 03:49:42 +0200

    programs component done

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,8 +1,31 @@
 import React from "react"
 import "./Programs.css"
+import RightArrow from "../../assets/rightArrow.png"
+import { programsData } from "../../data/programsData"
 
 const Programs = () => {
-  return <div>Programs</div>
+  return (
+    <div className="programs" id="programs">
+      <div className="programs__header">
+        <span className="stroke-text">Explore our</span>
+        <span>Programs</span>
+        <span className="stroke-text">to shape you</span>
+      </div>
+      <div className="programs__categories">
+        {programsData.map((program) => (
+          <div className="programs__categories__category">
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="programs__categories__category--join-now">
+              <span>Join Now</span>
+              <img src={RightArrow} alt="eight arrow" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Programs
